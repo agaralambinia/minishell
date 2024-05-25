4daf6a44c6439cca38bed48970ced4f045ea19c8
Osselian <rossemir11@gmail.com>
2024-05-25 21:26:14 +0400

    readline realized

--- osokin_src/minishell.h
+++ osokin_src/minishell.h
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishel.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sosokin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/22 10:43:29 by sosokin           #+#    #+#             */
-/*   Updated: 2024/05/25 19:04:34 by sosokin          ###   ########.fr       */
+/*   Created: 2024/05/22 10:25:28 by sosokin           #+#    #+#             */
+/*   Updated: 2024/05/25 21:22:40 by sosokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# define READLINE_LIBRARY
 
-int	main()
-{
-	char	*line;
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+//# include "history.h"
+//# include "readline.h"
 
-	line = readline("> ");
-	while (line)
-	{
-		add_history(line);
-		printf("You type %s, great...\n", line);
-		line = readline("> ");
-	}
-
-	return (0);
-}
+#endif
