6b05af067f31ffb2167a49002413d2b1409725bc
Osselian <rossemir11@gmail.com>
2024-05-30 14:02:29 +0400

    delete

--- osokin_src/minishell.h
+++ osokin_src/minishell.h
@@ -6,17 +6,18 @@
 /*   By: sosokin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 10:25:28 by sosokin           #+#    #+#             */
-/*   Updated: 2024/05/26 17:56:02 by sosokin          ###   ########.fr       */
+/*   Updated: 2024/05/30 13:51:08 by sosokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# define READLINE_LIBRARY
 
 # include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-//# include "history.h"
-//# include "readline.h"
+//# include <readline/readline.h>
+//# include <readline/history.h>
+# include "history.h"
+# include "readline.h"
 
 #endif
