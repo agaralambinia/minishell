4474fef7c10d6daf68913041a414d65eda6baa73
Osselian <rossemir11@gmail.com>
2024-06-09 15:50:23 +0400

    dead end

--- project/srcs/cmd_builder/bind_field.c
+++ project/srcs/cmd_builder/bind_field.c
@@ -6,7 +6,7 @@
 /*   By: sosokin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 20:46:48 by sosokin           #+#    #+#             */
-/*   Updated: 2024/06/02 19:08:23 by sosokin          ###   ########.fr       */
+/*   Updated: 2024/06/08 16:41:24 by sosokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 static void	setinout(t_cmd *com, char *word, char *field)
 {
+	t_list	*node;
+	t_redir	*redir;
+	
+	redir = (t_redir *)safe_malloc(sizeof(t_redir));
+	redir->path = word;
+	if (field[2])
+		redir->mode = 1;
+	node = ft_lstnew(redir);
 	if (field[1] == 'i')
-	{
-		com->input = word;
-		if (field[2])
-			com->inmode = 1;
-	}
+		ft_lstadd_back(&(com->redir_in), node);
 	else if (field[1] == 'o')
-	{
-		com->output = word; 
-		if (field[2])
-			com->outmode = 1;
-	}
+		ft_lstadd_back(&(com->redir_out), node);
 	field[1] = 0;
 	field[2] = 0;
 }
