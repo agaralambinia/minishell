2cf5a4c65d08503aa8a8aae6dfb527e46ddef089
Osselian <rossemir11@gmail.com>
2024-07-11 17:31:31 +0400

    cd fixed

--- project/srcs/utils/ft_list_replace.c
+++ project/srcs/utils/ft_list_replace.c
@@ -6,7 +6,7 @@
 /*   By: defimova <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 18:18:49 by defimova          #+#    #+#             */
-/*   Updated: 2024/07/10 17:38:02 by sosokin          ###   ########.fr       */
+/*   Updated: 2024/07/11 17:25:37 by sosokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@ int	ft_list_replace(t_envp *envp_var, char *old_var, char *new_var)
 	t_list	*temp;
 
 	temp = envp_var->envp_list;
-	while ((ft_strcmp((char *)(temp->content), old_var) != 0) && temp != NULL)
+	if (!old_var)
+	{
+		ft_lstadd_back(&(envp_var->envp_list), ft_lstnew(new_var));
+		return (SUCCESS);
+	}
+	while (temp != NULL && ft_strcmp((char *)(temp->content), old_var) != 0)
 	{
 		temp = temp -> next;
 	}
